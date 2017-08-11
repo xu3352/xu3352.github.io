11ba4bf49841678ae82c1bb34f078f97a0af19d8
xu3352 <xu3352@gmail.com>
2017-08-12 02:04:03 +0800

    启动 jekyll 时清掉日志；自动 kill 之前的进程

--- startup.sh
+++ startup.sh
@@ -2,7 +2,13 @@
 #author:xu3352
 #desc: start up jekyll
 
+# clear nohup.out
+echo "" > nohup.out
+
+# kill first
+ps aux|grep -v 'grep'|grep 'jekyll'|awk '{print $2}'| xargs kill -9
+
+# start up jekyll
 cd ~/blog
 nohup bundle exec jekyll serve --drafts --incremental --trace &
-
 
