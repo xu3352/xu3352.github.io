1ed557bbc0c7c95bfbfee95eedeb958994d4a476
xu3352 <xu3352@gmail.com>
2018-11-01 17:36:12 +0800

    内容层级支持自动展开和收起

--- assets/js/post_nav.js
+++ assets/js/post_nav.js
@@ -3,52 +3,88 @@
  *  依赖 zepto.min.js 等类jquery库
  *  v1.0 仅支持展示H1
  *  v1.1 支持H1~H6，小标题自动往后缩进
+ *  v1.2 默认只展示H1,H2; 其他的默认收起; 有子集的自动展开
  */
-
-/** 获取 H1~H6 所有元素 */
-function getHeadlineTags() {
-    var arrays = [];
-    $("*[id]").each(function(){
-        var tagName = $(this).prop("tagName");
-        if ($.inArray(tagName, hs) >= 0) {
-            // console.log(tagName)
-            arrays.push($(this));
-        }
-    });
-    return arrays;
-}
-
-/** 判断元素标题等级H1~H6，返回0~5，如果不是H1~H6，则返回-1 */
-function getHeadlineLevel(h) {
-    var tagName = $(h).prop("tagName");
-    return $.inArray(tagName, hs);
-}
-
-/** 生成目录列表 */
-function generateContentList(array) {
-    if (array.length > 1) {
-        var dom =  '<ul class="post_nav">'
-        for (var i = 0; i < array.length; i++) {
-            var $h1 = $(array[i]);
-            var level = getHeadlineLevel( $h1 );
-            var li_style = level <= 0 ? '': ' style="margin-left:'+(level*12)+'px"';
-            dom += '<li'+li_style+'><a style="color:#999" href="#'+ $h1.attr("id") +'">'+ $h1.text() +'</a></li>';
-        }
-        dom += '</ul> ';
-
-        // 边栏root [Published, Tags]
-        var $sideBar = $("ul.tag_box").parent();
-
-        // append dom
-        $sideBar.append('<h4>Content List</h4>');
-        $sideBar.append(dom);
+$(document).ready(function(){
+    /** 获取 H1~H6 所有元素 */
+    function getHeadlineTags() {
+        var arrays = [];
+        $("*[id]").each(function(){
+            var tagName = $(this).prop("tagName");
+            if ($.inArray(tagName, hs) >= 0) {
+                // console.log(tagName)
+                arrays.push($(this));
+            }
+        });
+        return arrays;
     }
-}
 
-// H1~H6 标签名数组
-var hs = ["H1", "H2", "H3", "H4", "H5", "H6"];
-// 找到所有 H1~H6 
-var array = getHeadlineTags();
-// 生成 Content List
-generateContentList(array);
+    /** 判断元素标题等级H1~H6，返回0~5，如果不是H1~H6，则返回-1 */
+    function getHeadlineLevel(h) {
+        var tagName = $(h).prop("tagName");
+        return $.inArray(tagName, hs);
+    }
+
+    /** 生成目录列表 */
+    function generateContentList(array) {
+        if (array.length > 1) {
+            var dom =  '<ul class="post_nav">'
+            for (var i = 0; i < array.length; i++) {
+                var $h1 = $(array[i]);
+                var level = getHeadlineLevel( $h1 );
+                var li_style = level <= 0 ? '': ' style="margin-left:'+(level*12)+'px"';
+                    li_style = ' data-level="'+(level+1)+'" is-show="'+(level+1<=2?1:0)+'"' + li_style;
+                dom += '<li'+li_style+'><a style="color:#999" href="#'+ $h1.attr("id") +'">'+ $h1.text() +'</a></li>';
+            }
+            dom += '</ul> ';
+
+            // 边栏root [Published, Tags]
+            var $sideBar = $("ul.tag_box").parent();
+
+            // append dom
+            $sideBar.append('<h4>Content List</h4>');
+            $sideBar.append(dom);
+        }
+    }
+
+    // h2 以下的子集全部隐藏
+    function resetShow() {
+        $(".post_nav li").each(function(){
+            if ($(this).attr("data-level") > 2) $(this).attr("is-show", 0);
+        });
+    }
+
+    // 子集展开
+    $("div.row").on("click", ".post_nav li a", function(){
+        var $a = $(this);
+        // 点中的加下划线
+        $(".post_nav a.active").removeClass("active");
+        $a.addClass("active");
+
+        // h1 不处理
+        var curr_level = $a.parent().attr("data-level");
+        if (curr_level <= 2) resetShow();
+        if (curr_level <= 1) return;
+       
+        // 没有下一个 || 下一个是同级别的 不处理
+        var $next = $a.parent().next("li");
+        if ($next.length == 0 || $next.attr("data-level") <= curr_level) return;
+
+        // 下级不是 h1 的情况
+        var cmp_level = $next.attr("data-level");
+
+        // 同一层级的展开
+        do {
+            $next.attr("is-show", 1);
+            $next = $next.next("li");
+        } while ($next.length > 0 && $next.attr("data-level") == cmp_level);
+    })
+
+    // H1~H6 标签名数组
+    var hs = ["H1", "H2", "H3", "H4", "H5", "H6"];
+    // 找到所有 H1~H6 
+    var array = getHeadlineTags();
+    // 生成 Content List
+    generateContentList(array);
+});
 
